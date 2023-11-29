8b5b7d5b386aa42237434899a6c8c81f8e019e72
40220321 <gohare10@qub.ac.uk>
2023-11-29 10:04:07 +0000

    added icons for bottom nav

--- app/src/main/java/com/example/addappt/MainActivity.kt
+++ app/src/main/java/com/example/addappt/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.addappt
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,16 +16,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.addappt.ui.theme.ADDapptTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ADDapptTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +33,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ADDapptTheme {
-        Greeting("Android")
-    }
 }
