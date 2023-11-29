8fa82bf5ea88d48edb918835ac6505019f2e7d17
40220321 <gohare10@qub.ac.uk>
2023-11-29 10:56:02 +0000

    added bottom Nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.addappt"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.addappt"
--- app/src/main/java/com/example/addappt/MainActivity.kt
+++ app/src/main/java/com/example/addappt/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.addappt.ui.theme.ADDapptTheme
 
@@ -22,11 +26,65 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ADDapptTheme {
-                Scaffold(
+                Scaffold (
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                label = {
+                                    Text("Home")
+                                },
+                                icon = {
+                                    Icon(painter = painterResource(id = R.drawable.ic_home), contentDescription = "Home Icon")
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                label = {
+                                    Text("Motivation")
+                                },
+                                icon = {
+                                    Icon(painter = painterResource(id = R.drawable.ic_calendar), contentDescription = "Calendar Icon")
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                label = {
+                                    Text("Add")
+                                },
+                                icon = {
+                                    Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "Add Icon")
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                label = {
+                                    Text("Stats")
+                                },
+                                icon = {
+                                    Icon(painter = painterResource(id = R.drawable.ic_graph), contentDescription = "Graph Icon")
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                label = {
+                                    Text("Profile")
+                                },
+                                icon = {
+                                    Icon(painter = painterResource(id = R.drawable.ic_profile), contentDescription = "Profile Icon")
+                                }
+                            )
+                        }
+                    },
+                    content = {
 
-                ) {
-
-                }
+                    }
+                )
             }
         }
     }
