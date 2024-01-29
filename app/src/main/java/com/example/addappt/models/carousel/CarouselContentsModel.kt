161d6f709d5390d28879ea59f78f5b97d80470c1
40220321 <gohare10@qub.ac.uk>
2024-01-29 09:08:11 +0000

    Added initial carousel to Home Screen

---
+++ app/src/main/java/com/example/addappt/models/carousel/CarouselContentsModel.kt
@@ -0,0 +1,7 @@
+package com.example.addappt.models.carousel
+
+data class CarouselContentsModel(
+    val title : String,
+    val icon : Int,
+    val text : String
+)
---
+++ app/src/main/java/com/example/addappt/models/carousel/CarouselInsetModel.kt
@@ -0,0 +1,5 @@
+package com.example.addappt.models.carousel
+
+data class CarouselInsetModel(
+    val carouselInfo : ArrayList<CarouselContentsModel>
+)
